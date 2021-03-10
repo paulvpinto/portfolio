1f8600845c539997450460c0dc2487c0457e7952
paulvpinto <paul_pinto@hotmail.ca>
2021-03-10 15:56:14 -0800

    Updated Text and Resume

--- src/Data.js
+++ src/Data.js
@@ -21,7 +21,7 @@ import GuidingSoulsImage4 from "./Images/GuidingSouls/GuidingSouls4.png"
 import GuidingSoulsImage5 from "./Images/GuidingSouls/GuidingSouls5.png"
 import GuidingSoulsImage6 from "./Images/GuidingSouls/GuidingSouls6.png"
 import GuidingSoulsImage7 from "./Images/GuidingSouls/GuidingSouls7.png"
-
+import GuidingSoulsImage8 from "./Images/GuidingSouls/GuidingSouls8.png"
 
 export default [
     {
@@ -39,7 +39,7 @@ export default [
                     fields: {
                         id: "1",
                         title: "Game Details",
-                        paragraph: "August 2020 \n \n Inner Tune is a 3D Rhythm Action Game Project where the main character must face emotion boss versions of themself in various dreamscape worlds. \n \n Role: Solo Developer \n \n I created the game in Unreal Engine 4, the music in Reaper, the character model in VRoid Studio, the environment in Blender and used animations, and objects from Mixamo and Epic Games' Paragon assets.",
+                        paragraph: "August 2020 \n \n Inner Tune is a 3D Rhythm Action Game Project where the protagonist must face emotion boss versions of themself in various dreamscape worlds. They must utilize the power to perform enhanced actions according to their own beat while dodging the attacks of the bosses that match with their song. \n \n Role: Solo Developer \n \n I developed the game using blueprints in Unreal Engine 4, the music and sounds in Reaper, the character model in VRoid Studio, the environment and other assets in Blender, and used animations and objects from Mixamo and Epic Games' Paragon assets.",
                         media:
                             {
                                 type: "video",
@@ -51,7 +51,7 @@ export default [
                     fields: {
                         id: "2",
                         title: "Process",
-                        paragraph: "One of the main challenges of creating this first demo was incorporating the rhythm mechanic into the combat without over complicating the core gameloop. The way I tackled this was designing the boss attack patterns to match the song and creating a enhanced action system that follows a seperate rhytmic pattern. This allowed the player to have cues for each system and not be as overwhemled during combat.",
+                        paragraph: "One of the main challenges of creating this first demo was incorporating the rhythm mechanic into the combat without over complicating the core gameloop. The way I tackled this was designing the boss attack patterns to match the song and creating a enhanced action system that follows a seperate rhytmic pattern. This allowed the player to have cues for each system and not be as overwhemled during combat. \n \n The way the enhanced action system works is that there is a constant drum beat that plays alongside the levels BGM, the player may perform an action such as jumping, dashing, and attacking at the same time as the beat to trigger an enhanced version of the performed action which uses no energy and has increased effects.",
                         media:
                             {
                                 type: "image",
@@ -63,7 +63,7 @@ export default [
                     fields: {
                         id: "3",
                         title: "Reflection",
-                        paragraph: "This first iteration was a great experience as I was able to learn a lot about the different roles of game developement and how each aspect of creating the game work together. After doing some internal balance testing and having friends try out the game, there is a lot of improve and further develop.",
+                        paragraph: "This first iteration was a great experience as I was able to learn a lot about the different roles of game developement and how each aspect of creating the game work together. After doing some internal balance testing and having friends try out the game, there is a lot of improve and further develop. \n \n In the future, I plan to create several bosses and levels that encompass a different emotion, creating music and designing the world and level to match each motif. The story follows the protagonist overcoming his overflowing emotions with the help of music. Some examples of upcoming bosses would be a red, angry boss which uses metal, rock, or rap, another one would be a yellow, happy boss which uses pop, or funk.",
                         media: 
                             {
                                 type: "gallery",
@@ -97,7 +97,7 @@ export default [
                     fields: {
                         id: "1",
                         title: "Game Details",
-                        paragraph: "Spring Semester 2018 \n \n Lumera is a 3D Action Role Playing Game where the summoned protagonist must save the world tree from the corrupted creatures set on destroying everything in their path. \n \n Role: Game Designer / Developer \n \n This was a term project that I created with a team in Unreal Engine 4. We had to get out of our comfort zone and learn many new skills to achieve our ambitions of creating a game in a engine we had no experience in.",
+                        paragraph: "Spring Semester 2018 \n \n Lumera is a 3D Action Role Playing Game where the summoned protagonist must save the world tree from the corrupted creatures set on destroying everything in their path. They must fight several bosses with the aid of a gun and sword bestowed by Lumera. Players will attack, dodge, and utilize a gauge system that will allow the player to attack with great power. \n \n Role: Game Designer / Programmer / 3D Modeler / Sound Designer \n \n This was a term project that I created with a team in Unreal Engine 4. With a thirst for knowledge and curiosity I worked on designing the level progression, and core gameplay, developed the models, animations, and ai for the Oni Brother bosses, scripted the players controls, attacks, and gauge system, work on the music and narrative system, as well as collaborating with my teammates on finishing different tasks.",
                         media:
                             {
                                 type: "image",
@@ -109,7 +109,7 @@ export default [
                     fields: {
                         id: "2",
                         title: "Process",
-                        paragraph: "One of the main challenges we faced while working on this game was achieving the milestones and requirements of the project while learning how to create the game in Unreal Engine 4 as many of only had experience in Unity. The way we tackled this was by having each member focus on what they were best at then merging code together after certain milestones. The component I focused on was developing the core game loop and creating the base code to incorporating the different requriments that we needed.",
+                        paragraph: "One of the main challenges we faced while working on this game was learning and developing our skills while trying to create the game that we originally envisioned. As many of us had not specialized into a particular skill except for the 2D artist on our team we did a lot of self reflection on what parts of creating a game we really loved doing. For me, one of my favourite parts was the ideation, and developing of the core concept, as well as balancing the experience of the boss fight for the player. \n \n Within our time constraint we were able to create a game demo with 5 varying boss fights, as well as create a short narrative with cutscenes to showcase the story we developed for this RPG. Along with developing the main unique mechanic of the game, the gauge system which allowed the player to interact with the world. The way the mechanic works is that the player can charge up an attack which will pop up a guage meter where if the player timed it correctly they would perform an enhanced attack that would cause different interactions with different objects within the world. ",
                         media:
                             {
                                 type: "image",
@@ -121,7 +121,7 @@ export default [
                     fields: {
                         id: "3",
                         title: "Reflection",
-                        paragraph: "After the turmoil and struggles of game development, we were quite satisfied with what we were able to create within the time frame of a semester as well as choosing to use an entirely new game engine. This experience gave a good insight to developing with constraints and working within a time crunch as well as introduced us to discovering a new set of skills that we didn't know we had.",
+                        paragraph: "Overall we really appreciate the time we took to learn a new engine and to work with 3D as it helped us develop our current skills as well as learn many new ones. This experience gave a good insight to developing with constraints and working within a time crunch. We had so many ideas that we wanted to incorporate but had to cut down due to time constraints but the proudest interaction that we were able to add in was during the boss battle with a giant stone golem. \n \n With the gauge system, the player was able to reflect back the rock that the golem threw if they were able to time their attack which looked very cool when it was achieved. Though very hard to achieve we originally wanted the allow the player to experience to discover different ways to go about completing the level and defeating the boss.",
                         media: 
                             {
                                 type: "gallery",
@@ -154,7 +154,7 @@ export default [
                     fields: {
                         id: "1",
                         title: "Game Details",
-                        paragraph: "You are a janitor of the Underworld. Hades threw a tantrum after Pirithous broke into the Underworld again, and now there are lost souls scattered in the deepest, darkest areas where the Soul Eaters reside. Unfortunately, it's not feeding time for another 5000 years, so Hades tossed you down there to clean up the place or be stuck there for eternity. What did you do to deserve this treatment? It’s not like you tried to steal his wife… \n \n This game is a creation for the Global Game Jam 2021.\n \n Role: Game Designer / Programmer \n \n https://agehm.itch.io/guiding-souls \n \n https://globalgamejam.org/2021/games/guiding-souls-6",
+                        paragraph: "Guiding Souls is a 2D puzzle game where the protagonist, a janitor of the underworld must find the lost souls of the dead and offer them to the portal to go to the next area. The player must collect these souls that will follow him and keep them safe from the many dangers and perils that the underworld commonly posseses.\n \n This game is a creation for the Global Game Jam 2021. I was able to assemble a team with a talented SFX Artist and a talented narrative designer which you can find more about on our itch.io page. Feel free to test out our game by downloading it from the official game jam page or our itch.io page.\n \n Role: Game Designer / Programmer \n \n https://agehm.itch.io/guiding-souls \n \n https://globalgamejam.org/2021/games/guiding-souls-6",
                         media:
                             {
                                 type: "video",
@@ -166,7 +166,7 @@ export default [
                     fields: {
                         id: "2",
                         title: "Process",
-                        paragraph: "Since this was my first game jam, there were many challenges with being unfamiliar with how the whole process is done. One of the biggest obstacles was the limitation of time, as you had to form a team, come up with an idea that fits the theme, and create a playable game within the time frame. But after finding some great team mates and asking help from the incredible event runners and volunteers, we were able to create a game matching the theme of Lost & Found.",
+                        paragraph: "Since this was my first game jam, there were many challenges with being unfamiliar with how the whole process is done. The first step I had to do was form a team as well as develop a game idea that matched the theme of Lost and Found. One of things that stuck to me was the Greek stories of the underworld, where souls are trapped with no sense of purpose, ideally fitting the role of lost. Since we decided to go with pixel art route we decided to turn the dark motif into a more comedic sense by introducing the protagonist as a janitor that has to clean up all the lost souls. \n \n The main mechanic that the player can play around with is the ability to throw the following souls forward as many of the souls move at a very slow pace. As the player finds different souls, enemies, and obstacles, the levels get harder and harder as the depths of the underworld get more complicated the deeper you delve. Players can also experience the story with some witty banter as the protagonist complains to himself about his situation and the world he is in.",
                         media:
                             {
                                 type: "image",
@@ -178,7 +178,7 @@ export default [
                     fields: {
                         id: "3",
                         title: "Reflection",
-                        paragraph: "This was a great experience, which helped push both my game design and scripting skills to the test. It has been a while since I worked with Unity but it was a great refresher and it taught me a lot of new things while creating the game. This experience gave me a lot of insight to all the different roles needed to creating a game. Overall this game jam was a great experience, and I look forward to joining more and meeting new people.",
+                        paragraph: "This was a great experience, which helped push both my game design and scripting skills to the test. It has been a while since I worked with Unity but it was a great refresher and it taught me a lot of new things while creating the game. This experience gave me a lot of insight to all the different roles needed to creating a game. \n \n If we were to further develop this game, we would've loved to add more obstacles and interactions as well as more types of souls with various behaviours as well as more kinds of enemies. Overall this game jam was a great experience, and I look forward to joining more gam jams and meeting new people.",
                         media: 
                             {
                                 type: "gallery",
@@ -188,7 +188,8 @@ export default [
                                     GuidingSoulsImage4,
                                     GuidingSoulsImage5,
                                     GuidingSoulsImage6,
-                                    GuidingSoulsImage7
+                                    GuidingSoulsImage7,
+                                    GuidingSoulsImage8
                                 ]
                             }
                         
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -22,6 +22,25 @@ const Container = Styled.div`
     }
 `;
 
+const Bio = Styled(Typography)`
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+    font-size: 2.1vh;
+    padding: 3.5rem;
+
+    ${props => props.theme.breakpoints.up("md")} {
+        grid-column: 2/6;
+        padding: 0rem 3.5rem;
+        font-size: 1.3vw;
+    }
+
+    ${props => props.theme.breakpoints.up("lg")} {
+        grid-column: 2/6;
+        padding: 0rem 3.5rem;
+        font-size: 1vw;
+    }
+`;
+
 const Tagline = Styled(Typography)`
     width: 100%;
     text-align: center;
@@ -30,41 +49,10 @@ const Tagline = Styled(Typography)`
     font-weight: 400;
     font-size: 1.5rem;
 
-    padding: 3.5rem;
-
     ${props => props.theme.breakpoints.up("md")} {
-        grid-column: 5/8;
-        grid-row: 1/2;
-        padding: 0rem;
+        grid-column: 6/8;
+        grid-row: 6/7;
     }
-`;
-
-const Bio = Styled(Typography)`
-    font-family: "Roboto", sans-serif;
-    font-weight: 300;
-    font-size: 2.1vh;
-    padding: 3.5rem;
-
-    ${props => props.theme.breakpoints.up("md")} {
-        grid-column: 1/4;
-        grid-row: 1/2;
-        padding: 2rem 3.5rem;
-    }
-
-`;
-
-const Quote = Styled(Typography)`
-    font-family: "Roboto", sans-serif;
-    font-weight: 300;
-    font-size: 2.1vh;
-    padding: 3.5rem;
-
-    ${props => props.theme.breakpoints.up("md")} {
-        grid-column: 2/6;
-        grid-row: 3/4;
-        padding: 2rem 3.5rem;
-    }
-
 `;
 
 const ImageContainer1 = Styled.div`
@@ -73,8 +61,8 @@ const ImageContainer1 = Styled.div`
     height: 40vw;
     
     ${props => props.theme.breakpoints.up("md")} {
-        grid-column: 6/9;
-        grid-row: 2/9;
+        grid-column: 6/8;
+        grid-row: 1/6;
         width: 100%;
         height: 100%;
     }
@@ -90,17 +78,23 @@ function About(props) {
             <ImageContainer1></ImageContainer1>
 
             <Tagline>
-                    I love to create games.
+            I love to create games.
             </Tagline>
 
             <Bio>
-                I graduated with a BSc in Interactive Arts and Technology learning many skills in the fields of digital media and design. This has enabled me to be more relevant and insightful when working with other members of a team when collaborating on a project.
+            Born and raised in Vancouver, I was fortunate enough to play many game consoles and handhelds of the generations. From the gameboy, to the dreamcast, to the playstation, to the xbox, I fell in love with playing video games. But the love didn't just stop at digital gaming, as I had been exposed to Tabletop games and card games splitting my heart in two.
 
+            <br></br>   
+            <br></br>  
+
+            Delving into the world of computer science, I had discovered I could use my skills of creativity and logic to create games and experiences for myself and others to share. As I shifted my career towards Game Design, I graduated with a BSc in Interactive Arts and Technology learning many skills in the fields of digital media and design. 
+
+            <br></br>   
+            <br></br>  
+
+            This has enabled me to be more relevant and insightful when working with other members of a team when collaborating on a project. As well as procure many different skills that have aided me in creating demos and prototypes of games, as well as create concepts, test, and balance a variety of games and projects I have worked on.
             </Bio>
 
-            <Quote>
-                Games to me are exploring the endless possibilities of design and creativity.
-            </Quote>
         </Container>
 
         <FeaturedWorks></FeaturedWorks>
